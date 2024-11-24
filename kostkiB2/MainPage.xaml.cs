fc7cd550980bce2301599dfff39d86fb9da294d5
bartu <bartus.kon@gmail.com>
2024-11-24 14:29:58 +0100

    To jest na 4

--- kostkiB2/MainPage.xaml.cs
+++ kostkiB2/MainPage.xaml.cs
@@ -13,9 +13,31 @@
         {
             Random r = new Random();
 
-            int diceRoll = r .Next(1, 7);
+            int diceRoll = 6;
 
-            CounterBtn.Text = "Wynik rzutu:   " + diceRoll. ToString();
+            if(K4.IsChecked)
+            {
+                diceRoll = r.Next(1, 5);
+            }
+
+            if (K6.IsChecked)
+            {
+                diceRoll = r.Next(1, 7);
+            }
+
+            if (K10.IsChecked)
+            {
+                diceRoll = r.Next(1, 11);
+            }
+
+            if (K12.IsChecked)
+            {
+                diceRoll = r.Next(1, 13);
+            }
+
+            RollLabel.Text = "Wynik rzutu:   " + diceRoll. ToString();
+
+
         }
     }
 
